93010494d7345439a3d9a4110a0b501f30f0efe3
Aravind Pai <dragondive@outlook.in>
2021-08-03 10:07:36 +0530

    improved print messages and cleanup

--- stocks_complexity.py
+++ stocks_complexity.py
@@ -1,162 +1,91 @@
 import pandas
-import matplotlib
+from matplotlib import pyplot
 from math import exp
 
 
 def portfolio_complexity(proportions):
     proportions.sort()
-    weight_exponent = 0.01
-
-    weighted_proportions = [
-        p * exp(weight_exponent * index) for index, p in enumerate(proportions)
-    ]
-
+    exponent_tuning_factor = 0.01
 
     total = 0
-    [total := total + x * exp(weight_exponent * index) for index, x in enumerate(proportions)]
+    [total := total + value * exp(exponent_tuning_factor * index) for index, value in enumerate(proportions)]
     
     return total
+    
+def update_proportion_in_portfolio(portfolio):
+    amount_sum = portfolio['Amount'].sum()
+    print("amount_sum: " + str(amount_sum))
+    portfolio['Proportion'] = portfolio['Amount'] / amount_sum
 
-transactions_list = pandas.read_excel('test.xlsx')
-# print(transactions_list)
+transactions_list = pandas.read_excel('test1.xlsx')
+print("input transactions list:\n" + str(transactions_list))
 
 sorted_by_date_list = transactions_list.sort_values(by='Date')
-print(sorted_by_date_list)
+print("transactions list sorted ascending by date:\n" + str(sorted_by_date_list))
 
 ongoing_date = None
-ongoing_portfolio = pandas.DataFrame()
-out_portfolio = pandas.DataFrame()
-
-plot_data = pandas.DataFrame(columns=['Date', 'Complexity'])
+daily_portfolio = pandas.DataFrame()
+full_portfolio = pandas.DataFrame()
+plot_data = pandas.DataFrame()
 
 for index, row in sorted_by_date_list.iterrows():
     date = row['Date']
+    print("date: " + str(date))
     
     if date != ongoing_date:
-        print("Date change from: " + str(ongoing_date) + " to: " + str(date))
-        print("\nOngoing portfolio: \n" + str(ongoing_portfolio))
+        print("date changed from: " + str(ongoing_date) + " to: " + str(date))
+        print("previous date: " + str(ongoing_date) + " daily portfolio:\n" + str(daily_portfolio))
         
-        # try: # ugly coding by exception, to be removed later
-        if 'Amount' in ongoing_portfolio.columns:
-            sum_amount = ongoing_portfolio['Amount'].sum()
-            print("sum_amount = " + str(sum_amount))
-            ongoing_portfolio['Proportion'] = ongoing_portfolio['Amount']/sum_amount
+        try: # ugly coding by exception, to be removed later
+            update_proportion_in_portfolio(daily_portfolio)
+            print("previous date: " + str(ongoing_date) +  " daily portfolio after updating proportion:\n" + str(daily_portfolio))
             
-            print("ongoing_portfolio after proportion:\n" + str(ongoing_portfolio))
-            
-            complexity = portfolio_complexity(ongoing_portfolio['Proportion'].tolist())
-            print("complexity = " + str(complexity))
+            complexity = portfolio_complexity(daily_portfolio['Proportion'].tolist())
+            print("previous date: " + str(ongoing_date) + " complexity: " + str(complexity))
             
             plot_data = plot_data.append({'Date' : ongoing_date, 'Complexity' : complexity}, ignore_index = True)
-        # except KeyError:
-            # pass
+        except KeyError:
+            pass
 
-        out_portfolio = out_portfolio.append(ongoing_portfolio)    
-        ongoing_portfolio.replace({ongoing_date: date}, inplace=True)
+        full_portfolio = full_portfolio.append(daily_portfolio)
+        print("after portfolio update for date: " + str(ongoing_date) + " full portfolio:\n" + str(full_portfolio))
+        
+        daily_portfolio.replace({ongoing_date: date}, inplace=True)
         ongoing_date = date
         
     try: # todo remove this try except later
         name = row['Name']
-        print("name = " + name)
-        mask = (ongoing_portfolio['Name'] == name)
-        # mask = (sorted_by_date_list['Name'] == name)
-        print("mask: \n" + str(mask))
-        print("\nmask empty: " + str(mask.empty))
-        print("\nmask size: " + str(mask.size))
-        
-        print("\nongoing_portfolio[mask] =\n" + str(ongoing_portfolio[mask]))
+        print("stock name: " + name)
 
-        masked_rows = ongoing_portfolio[mask]
+        mask = (daily_portfolio['Name'] == name)
+        masked_rows = daily_portfolio[mask]
+        print("masked_rows in daily_portfolio =\n" + str(masked_rows))
+
         if masked_rows.empty == True: # this is a new Name
             print("mask is empty, appending new row")
-            ongoing_portfolio = ongoing_portfolio.append(row)
+            daily_portfolio = daily_portfolio.append(row)
         else:
             print("mask is not empty, updating existing row")
             amount = row['Amount']
-            # ongoing_portfolio['Amount'][mask] = 0
-            ongoing_portfolio.loc[mask, 'Amount'] += amount
+            daily_portfolio.loc[mask, 'Amount'] += amount
+            print("for date: " + str(ongoing_date) + " stock name: " + str(name) + " amount: " + str(amount) + " updated amount: " + str(daily_portfolio.loc[mask, 'Amount'].values[0]))
 
-            # print("amount: " + str(amount) + " ongoing_amount: " + str(ongoing_amount)) 
-            # total = amount + ongoing_amount
-            # print("total: " + total)
-        # print("mask = ", ongoing_portfolio['Name'] == name)
     except KeyError: # ugly code, coding by exception should be removed
-        ongoing_portfolio = ongoing_portfolio.append(row)
-    # matching_row = ongoing_portfolio[mask]
+        print("mask is empty, appending new row")
+        daily_portfolio = daily_portfolio.append(row)
 
 
-print("ongoing portfolio:\n" + str(ongoing_portfolio))
+print("previous date: " + str(ongoing_date) + " daily portfolio:\n" + str(daily_portfolio))
+update_proportion_in_portfolio(daily_portfolio)
 
-sum_amount = ongoing_portfolio['Amount'].sum()
-ongoing_portfolio['Proportion'] = ongoing_portfolio['Amount']/sum_amount
-print("ongoing portfolio after proportion:\n" + str(ongoing_portfolio))
+complexity = portfolio_complexity(daily_portfolio['Proportion'].tolist())
+print("previous date: " + str(ongoing_date) + " complexity: " + str(complexity))
 
-complexity = portfolio_complexity(ongoing_portfolio['Proportion'].tolist())
 plot_data = plot_data.append({'Date' : ongoing_date, 'Complexity' : complexity}, ignore_index = True)
+print("plot_data =\n" + str(plot_data))
 
-print("plot_data:\n" + str(plot_data))
-
-out_portfolio = out_portfolio.append(ongoing_portfolio)    
-print("out_portfolio:\n" + str(out_portfolio))
+full_portfolio = full_portfolio.append(daily_portfolio)    
+print("after portfolio update for date: " + str(ongoing_date) + " full portfolio:\n" + str(full_portfolio))
 
 plot_data.plot.line(x='Date', y='Complexity')
-matplotlib.pyplot.show()
-
-# ongoing_date = None
-# ongoing_portfolio = pandas.DataFrame()
-
-# out_portfolio = pandas.DataFrame()
-
-# for index, row in sorted_by_date_list.iterrows():
-    # # print(index, row)
-    # date = row['Date']
-    # if date != ongoing_date:
-        # print ("Date has changed to " + str(date))
-        
-        # out_portfolio = out_portfolio.append(ongoing_portfolio)
-        
-        # print("ongoing_date: " + str(ongoing_date) + " date: " + str(date))
-        # ongoing_portfolio.replace({ongoing_date: date}, inplace=True)
-        # print("ongoing_portfolio:\n" + str(ongoing_portfolio))
-        
-        # ongoing_date = date
-        
-    # # print("row:\n" + str(row))
-    # ongoing_portfolio = ongoing_portfolio.append(row)
-    # name = row['Name']
-    # print("Name: " + name)
-    
-    # try: 
-        # mask = ongoing_portfolio['Name'] == name
-        # print("mask: \n" + str(ongoing_portfolio[mask]))
-        
-        # amount = row['Amount']
-        # x = ongoing_portfolio[mask]['Amount'] + amount
-        # print("x = " + x)
-    # except KeyError:
-        # pass
-
-    # # print("ongoing portfolio:\n" + str(ongoing_portfolio))
-    # print("\n")
-        
-   
-# out_portfolio = out_portfolio.append(ongoing_portfolio) 
-# print("portfolio:" + str(out_portfolio))
-        
-        
-        
-    
-
-# sorted_list = transactions_list.sort_values(by=['Name', 'Date'])
-# print(sorted_list)
-
-# sorted_list['Total'] = sorted_list.groupby('Name').cumsum()
-# print(sorted_list)
-
-# sorted_by_date = sorted_list.sort_values(by='Date')
-# print(sorted_by_date)
-
-# print(sorted_by_date.groupby('Date').sum())
-
-
-    
+pyplot.show()
