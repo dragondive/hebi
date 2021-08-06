80ab251efb35d4401e127417240f888064154b57
Aravind Pai <dragondive@outlook.in>
2021-08-06 21:34:45 +0530

    Resolved several bugs, see details below.
    - augment startswith match with fuzzy search again to improve company name matches
    - return symbol from both exchanges to improve chance of obtaining closing price
      - if listed on first exchange later but already listed on other exchange
      - if data not available for specific date on first exchange
    - if data not available, calculate two week average as closing price
    - ignore NaN values while calculating complexity

--- multibeggar.py
+++ multibeggar.py
@@ -1,43 +1,58 @@
 import pandas
 import yfinance as yf
+from fuzzywuzzy import fuzz
 
-        
+
 def get_stock_symbol_from_nse(company_name, with_suffix=False):
     if not hasattr(get_stock_symbol_from_nse, 'stock_symbol_name_map'): # ugly code, will be rewritten more cleanly
         get_stock_symbol_from_nse.stock_symbol_name_map = pandas.read_csv('equity_nse.csv', usecols=['SYMBOL', 'NAME OF COMPANY'])
-    
-    matching_mask = (get_stock_symbol_from_nse.stock_symbol_name_map['NAME OF COMPANY'].str.startswith(company_name))
-    matching_row = get_stock_symbol_from_nse.stock_symbol_name_map[matching_mask]
-    if matching_row.empty == True:
-        return None
-    
-    stock_symbol = matching_row['SYMBOL'].values[0]
-    
-    if with_suffix == True:
-        stock_symbol += ".NS"
         
+    matching_mask_startswith = (get_stock_symbol_from_nse.stock_symbol_name_map['NAME OF COMPANY'].str.startswith(company_name))
+    matching_row_startswith = get_stock_symbol_from_nse.stock_symbol_name_map[matching_mask_startswith]
+    
+    stock_symbol = None
+    if matching_row_startswith.empty != True:
+        stock_symbol = matching_row_startswith['SYMBOL'].values[0]
+    else:
+        matching_mask_fuzzy = get_stock_symbol_from_nse.stock_symbol_name_map.apply(lambda x: fuzz.token_sort_ratio(x['NAME OF COMPANY'], company_name), axis=1)
+        qualified_rows = matching_mask_fuzzy[lambda x: x > 75]
+        if qualified_rows.empty != True:
+            best_matching_row = get_stock_symbol_from_nse.stock_symbol_name_map.loc[qualified_rows.idxmax()]
+            stock_symbol = best_matching_row['SYMBOL']
+            
+    if with_suffix == True and stock_symbol is not None:
+        stock_symbol += '.NS'
+    
+    print('nse company_name: ' + str(company_name) + ' symbol: ' + str(stock_symbol))
     return stock_symbol
 
 
 def get_stock_symbol_from_bse(company_name, with_suffix=False):
     if not hasattr(get_stock_symbol_from_bse, 'stock_symbol_name_map'): # ugly code, will be rewritten more cleanly
         get_stock_symbol_from_bse.stock_symbol_name_map = pandas.read_csv('equity_bse.csv', usecols=['Security Name', 'Security Id'])
-    
-    matching_mask = (get_stock_symbol_from_bse.stock_symbol_name_map['Security Name'].str.startswith(company_name))
-    matching_row = get_stock_symbol_from_bse.stock_symbol_name_map[matching_mask]
-    if matching_row.empty == True:
-        return None
-    
-    stock_symbol = matching_row['Security Id'].values[0]
-    
-    if with_suffix == True:
-        stock_symbol += ".BO"
         
+    matching_mask_startswith = (get_stock_symbol_from_bse.stock_symbol_name_map['Security Name'].str.startswith(company_name))
+    matching_row_startswith = get_stock_symbol_from_bse.stock_symbol_name_map[matching_mask_startswith]
+    
+    stock_symbol = None
+    if matching_row_startswith.empty != True:
+        stock_symbol = matching_row_startswith['Security Id'].values[0]
+    else:
+        matching_mask_fuzzy = get_stock_symbol_from_bse.stock_symbol_name_map.apply(lambda x: fuzz.token_sort_ratio(x['Security Name'], company_name), axis=1)
+        qualified_rows = matching_mask_fuzzy[lambda x: x > 75]
+        if qualified_rows.empty != True:
+            best_matching_row = get_stock_symbol_from_bse.stock_symbol_name_map.loc[qualified_rows.idxmax()]
+            stock_symbol = best_matching_row['Security Id']
+            
+    if with_suffix == True and stock_symbol is not None:
+        stock_symbol += '.BO'
+    
+    print('bse company_name: ' + str(company_name) + ' symbol: ' + str(stock_symbol))
     return stock_symbol
 
 
 def fixup_company_names(transactions_list):
-    fixup_company_names_map = pandas.read_csv('fixup_company_names.csv')
+    fixup_company_names_map = pandas.read_csv('fixup_company_names2.csv')
     
     for index, row in fixup_company_names_map.iterrows():
         # print(row['Actual Name'] + ' : ' + row['Fixed Name'])
@@ -45,38 +60,75 @@ def fixup_company_names(transactions_list):
 
 
 def get_stock_symbol(company_name, with_suffix=False):
-    stock_symbol = get_stock_symbol_from_nse(company_name, with_suffix)
-    if stock_symbol is None:
-        stock_symbol = get_stock_symbol_from_bse(company_name, with_suffix)
+    stock_symbol = [get_stock_symbol_from_nse(company_name, with_suffix),
+                    get_stock_symbol_from_bse(company_name, with_suffix)]
     
     return stock_symbol
 
 
 def get_closing_price(stock_symbol, date):
+    print('stock_symbol: ' + str(stock_symbol))
     start_date = pandas.to_datetime(date)
     end_date = start_date + pandas.Timedelta(days=1)
+    
+    for symbol in stock_symbol:
+        print('symbol: ' + str(symbol))
+        
+        if symbol is None:
+            continue
+
+        ticker = yf.Ticker(symbol)
+        print('ticker type:' + str(type(ticker)))
+        # exit(11)
+        stock_data = ticker.history(start=start_date, end=end_date)
+        
+        if stock_data.empty == False:
+            closing_price = stock_data['Close'].values[0]
+            print('found closing price: ' + str(closing_price))
+            return closing_price
+            
+    start_date_one_week_before = start_date - pandas.Timedelta(days=7)
+    end_date_one_week_after = start_date + pandas.Timedelta(days=7)
+    
+    for symbol in stock_symbol:
+        if symbol is None:
+            continue
+
+        ticker = yf.Ticker(symbol)
+        stock_data_two_weeks = ticker.history(start=start_date_one_week_before, end=end_date_one_week_after)
+        
+        if stock_data_two_weeks.empty == False:
+            closing_price = stock_data_two_weeks['Close'].mean()
+            print('closing price from 2 week average: ' + str(closing_price))
+            return closing_price
+        
+    return None
+            
+        
+    # start_date = pandas.to_datetime(date)
+    # end_date = start_date + pandas.Timedelta(days=1)
     # print('stock_symbol: ' + stock_symbol + ' start_date: ' + str(start_date) + ' end_date: ' + str(end_date))
     # print('date type: ' + str(type(date)))
-    # exit(1)
+    # # exit(1)
     
-    ticker = yf.Ticker(stock_symbol)
-    stock_data = ticker.history(start=start_date, end=end_date)
-    
-    try:
-        closing_price = stock_data['Close'].values[0]
-    except IndexError:
-        start_date_one_week_before = start_date - pandas.Timedelta(days=7)
-        end_date_one_week_after = start_date + pandas.Timedelta(days=7)
+    # ticker = yf.Ticker(stock_symbol)
+    # stock_data = ticker.history(start=start_date, end=end_date)
+    # print(str(stock_data))
+    # try:
+        # closing_price = stock_data['Close'].values[0]
+    # except IndexError:
+        # start_date_one_week_before = start_date - pandas.Timedelta(days=7)
+        # end_date_one_week_after = start_date + pandas.Timedelta(days=7)
         
-        stock_data_two_weeks = ticker.history(start=start_date_one_week_before, end=end_date_one_week_after)
-        # print('close data type: ' + str(type(stock_data_two_weeks['Close'])))
-        # print('close data:\n' + str(stock_data_two_weeks['Close']))
+        # stock_data_two_weeks = ticker.history(start=start_date_one_week_before, end=end_date_one_week_after)
+        # # print('close data type: ' + str(type(stock_data_two_weeks['Close'])))
+        # # print('close data:\n' + str(stock_data_two_weeks['Close']))
         
-        closing_price = stock_data_two_weeks['Close'].mean()
+        # closing_price = stock_data_two_weeks['Close'].mean()
     
-    # print('closing_price: ' + str(closing_price))
-    return closing_price
-    # return yf.Ticker(stock_symbol).history(start=start_date, end=end_date)['Close'].values[0]
+    # # print('closing_price: ' + str(closing_price))
+    # return closing_price
+    # # return yf.Ticker(stock_symbol).history(start=start_date, end=end_date)['Close'].values[0]
     
     
 # print('close = ' + str(yf.Ticker("BAJFINANCE.NS").history(start="2021-07-01", end="2021-07-02")['Close'].values[0]))
--- stocks_complexity.py
+++ stocks_complexity.py
@@ -31,16 +31,16 @@ def compute_value_and_update_proportion(portfolio):
     portfolio['Proportion'] = portfolio['Value'] / value_sum
 
 
-transactions_list = pandas.read_excel('transactions_20210802_reduced.xlsx')
+transactions_list = pandas.read_excel('transactions_20210802_reduced_again.xlsx')
 
 multibeggar.fixup_company_names(transactions_list)
-print('input transactions list after fixup of company names:\n' + str(transactions_list))
+# print('input transactions list after fixup of company names:\n' + str(transactions_list))
 
 transactions_list['Symbol'] = transactions_list.apply(lambda x: multibeggar.get_stock_symbol(x['Name'], with_suffix=True), axis=1)
-print('transactions_list after updating symbols:\n' + str(transactions_list))
+# print('transactions_list after updating symbols:\n' + str(transactions_list))
 
 sorted_by_date_list = transactions_list.sort_values(by='Date')
-print('transactions list sorted ascending by date:\n' + str(sorted_by_date_list))
+# print('transactions list sorted ascending by date:\n' + str(sorted_by_date_list))
 
 ongoing_date = None
 daily_portfolio = pandas.DataFrame()
@@ -59,7 +59,7 @@ for index, row in sorted_by_date_list.iterrows():
             compute_value_and_update_proportion(daily_portfolio)
             print('previous date: ' + str(ongoing_date) +  ' daily portfolio after updating value and proportion:\n' + str(daily_portfolio))
             
-            complexity = portfolio_complexity(daily_portfolio['Proportion'].tolist())
+            complexity = portfolio_complexity(daily_portfolio['Proportion'].dropna().tolist())
             print('previous date: ' + str(ongoing_date) + ' complexity: ' + str(complexity))
             
             plot_data = plot_data.append({'Date' : ongoing_date, 'Complexity' : complexity}, ignore_index = True)
@@ -78,26 +78,26 @@ for index, row in sorted_by_date_list.iterrows():
 
         mask = (daily_portfolio['Name'] == name)
         masked_rows = daily_portfolio[mask]
-        print('masked_rows in daily_portfolio =\n' + str(masked_rows))
+        # print('masked_rows in daily_portfolio =\n' + str(masked_rows))
 
         if masked_rows.empty == True: # this is a new Name
-            print('mask is empty, appending new row')
+            # print('mask is empty, appending new row')
             daily_portfolio = daily_portfolio.append(row)
         else:
-            print('mask is not empty, updating existing row')
+            # print('mask is not empty, updating existing row')
             shares = row['Shares']
             daily_portfolio.loc[mask, 'Shares'] += shares
-            print('for date: ' + str(ongoing_date) + ' stock name: ' + str(name) + ' shares: ' + str(shares) + ' updated shares: ' + str(daily_portfolio.loc[mask, 'Shares'].values[0]))
+            # print('for date: ' + str(ongoing_date) + ' stock name: ' + str(name) + ' shares: ' + str(shares) + ' updated shares: ' + str(daily_portfolio.loc[mask, 'Shares'].values[0]))
 
     except KeyError: # ugly code, coding by exception should be removed
-        print('mask is empty, appending new row')
+        # print('mask is empty, appending new row')
         daily_portfolio = daily_portfolio.append(row)
 
 
 print('previous date: ' + str(ongoing_date) + ' daily portfolio:\n' + str(daily_portfolio))
 compute_value_and_update_proportion(daily_portfolio)
 
-complexity = portfolio_complexity(daily_portfolio['Proportion'].tolist())
+complexity = portfolio_complexity(daily_portfolio['Proportion'].dropna().tolist())
 print('previous date: ' + str(ongoing_date) + ' complexity: ' + str(complexity))
 
 plot_data = plot_data.append({'Date' : ongoing_date, 'Complexity' : complexity}, ignore_index = True) 
@@ -109,8 +109,8 @@ print('after portfolio update for date: ' + str(ongoing_date) + ' full portfolio
 
 sorted_by_date_list.to_excel('test2_sorted_by_date.xlsx')
 full_portfolio.to_excel('test2_full_portfolio.xlsx')
-plot_data.to_excel('test2_plot_data.xlsx')
+log_file.close()
 
+plot_data.to_excel('test2_plot_data.xlsx')
 plot_data.plot.line(x='Date', y='Complexity')
 pyplot.show()
-log_file.close()
