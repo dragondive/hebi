df98a58647ba45844a8e93e19642ec86ba895bd6
Aravind Pai <dragondive@outlook.in>
2021-12-08 19:25:21 +0530

    Move companies info from multibeggar to dalalstreet [fix #25]

--- multibeggar/dalalstreet.py
+++ multibeggar/dalalstreet.py
@@ -12,59 +12,72 @@ class StockExchange(Enum):
     def __str__(self):
         return self.value
 
-
-class StockExchangeInfo:
-    def __init__(self, companies_data_file_path, symbol_header, company_name_header, exchange_suffix):
+class CompaniesInfo:
+    def __init__(self):
         # todo self: these log files should be managed better, this is a temporary hack to "reuse" the multibeggar.log
         logging.basicConfig(format='%(levelname)s: %(message)s [%(funcName)s():%(lineno)s - %(asctime)s]', datefmt='%Y/%m/%d %I:%M:%S %p', handlers=[logging.FileHandler(os.path.join(os.getcwd(), 'output', 'multibeggar.log'), mode='w')])
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
 
-        self.symbol_to_name = pandas.read_csv(companies_data_file_path, usecols=[symbol_header, company_name_header])
-        self.symbol_header = symbol_header
-        self.company_name_header = company_name_header
-        self.exchange_suffix = exchange_suffix
+        script_dir = os.path.dirname(__file__)
 
-    def get_symbol(self, company_name, with_suffix=True):
-        symbol_list = self.get_symbols_for_company_name_starting_with(company_name, with_suffix)
+        # The column names are repeated below inside the [[]] to rearrange the columns in required order.
+        # This is the officially documented approach in pandas.read_csv to avoid keeping the column order from the input.
+        self.stocks_info_map = {
+            StockExchange.NSE: pandas.read_csv(os.path.join(script_dir, 'data', 'equity_nse.csv'), usecols=['NAME OF COMPANY', 'SYMBOL'])[['NAME OF COMPANY', 'SYMBOL']],
+            StockExchange.BSE: pandas.read_csv(os.path.join(script_dir, 'data', 'equity_bse.csv'), usecols=['Security Name', 'Security Id'])[['Security Name', 'Security Id']],
+        }
+
+        self.company_name_header = 'company_name'
+        self.stock_symbol_header = 'stock_symbol'
+
+        # read_csv() does not support renaming the columns while reading in the data, so do it separately below.
+        for name, info in self.stocks_info_map.items():
+            info.columns = [self.company_name_header, self.stock_symbol_header]
+        # print(self.stocks_info_map)
+
+    def get_symbols(self, company_name):
+        # return tuples consisting of the stock symbol along with the exchange name
+        symbol_list = [(symbol, exchange_name) for exchange_name, info in self.stocks_info_map.items() if (symbol := self.get_symbol(company_name, exchange_name)) is not None]
+        self.logger.info('company_name: %s -> symbol: %s', company_name, symbol)
+        return symbol_list
+
+    def get_symbol(self, company_name, exchange_name):
+        symbol_list = self.get_symbols_for_company_name_starting_with(company_name, exchange_name)
 
         if len(symbol_list) == 1:
             symbol = symbol_list[0]
         else:
-            symbol = self.get_symbol_for_company_name_best_matching_with(company_name, with_suffix)
+            symbol = self.get_symbol_for_company_name_best_matching_with(company_name, exchange_name)
 
-        self.logger.info('company_name: %s -> symbol: %s', company_name, symbol)
+        self.logger.info('company_name: %s, exchange_name: %s -> symbol: %s', company_name, exchange_name, symbol)
         return symbol
 
-    def get_symbols_for_company_name_starting_with(self, company_name, with_suffix=True):
-        matching_mask = self.symbol_to_name[self.company_name_header].str.startswith(company_name)
-        matching_row = self.symbol_to_name[matching_mask]
-        symbol_list = matching_row[self.symbol_header].array
+    def get_symbols_for_company_name_starting_with(self, company_name, exchange_name):
+        name_to_symbol = self.stocks_info_map[exchange_name]
+        matching_mask = name_to_symbol[self.company_name_header].str.startswith(company_name)
+        matching_row = name_to_symbol[matching_mask]
+        symbol_list = matching_row[self.stock_symbol_header].array
 
-        if with_suffix:
-            symbol_list = [symbol + self.exchange_suffix for symbol in matching_row[self.symbol_header].array]
-
-        self.logger.info('company_name: %s -> symbol_list...\n%s', company_name, symbol_list)
+        self.logger.info('company_name: %s, exchange_name: %s -> symbol_list...\n%s', company_name, exchange_name, symbol_list)
         return symbol_list
 
-    def get_symbol_for_company_name_best_matching_with(self, company_name, with_suffix=True):
-        match_ratios = self.symbol_to_name.apply(lambda row: fuzz.token_sort_ratio(row[self.company_name_header], company_name), axis=1)
+    def get_symbol_for_company_name_best_matching_with(self, company_name, exchange_name):
+        name_to_symbol = self.stocks_info_map[exchange_name]
+        match_ratios = name_to_symbol.apply(lambda row: fuzz.token_sort_ratio(row[self.company_name_header], company_name), axis=1)
         qualified_rows = match_ratios[lambda x: x >= 75]
-        self.logger.debug('company_name: %s -> qualified_rows...\n%s', company_name,
-                          pandas.concat([self.symbol_to_name.loc[qualified_rows.index],
+        self.logger.debug('company_name: %s, exchange_name: %s -> qualified_rows...\n%s', company_name, exchange_name,
+                          pandas.concat([name_to_symbol.loc[qualified_rows.index],
                                         match_ratios.loc[qualified_rows.index].rename('match_ratio')],
                                         axis=1).to_string())
 
         try:
-            best_matching_row = self.symbol_to_name.loc[qualified_rows.idxmax()]
+            best_matching_row = name_to_symbol.loc[qualified_rows.idxmax()]
         except ValueError:
-            self.logger.warning('company_name: %s -> no symbol matched!', company_name)
+            self.logger.warning('company_name: %s, exchange_name: %s -> no symbol matched!', company_name, exchange_name)
             return None  # todo self: raise exception instead and let caller decide what to do
         else:
-            symbol = best_matching_row[self.symbol_header]
+            symbol = best_matching_row[self.stock_symbol_header]
 
-            if with_suffix:
-                symbol += self.exchange_suffix
-
-            self.logger.info('company_name: %s -> symbol: %s', company_name, symbol)
+            self.logger.info('company_name: %s, exchange_name: %s -> symbol: %s', company_name, exchange_name, symbol)
             return symbol
