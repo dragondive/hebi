811b17a3055ff82db7107c7e308fc5362b5eb13b
Aravind Pai <dragondive@outlook.in>
2022-12-24 11:59:59 +0530

    delete old implementation to start from scratch

--- multibeggar/tests/multibeggar_use.py
+++
@@ -1,8 +0,0 @@
-import os
-from multibeggar.multibeggar import Multibeggar
-
-mb = Multibeggar()
-mb.load_transactions_from_excel_file(os.path.join(os.getcwd(), 'input', 'test_transactions_shuffled_x_large.xlsx'))
-mb.plot_portfolio_complexity()
-
-# mb.get_nse_symbol('Motilal Oswal MOSt Shares NASDAQ-100 ETF')
